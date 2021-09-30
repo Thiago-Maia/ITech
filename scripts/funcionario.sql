7a5d53faf7bc3d820d56b94cb1cfc34bc4a48907
Thiago-Maia <72457726+Thiago-Maia@users.noreply.github.com>
2021-09-30 20:08:43 -0300

    Add files via upload

---
+++ scripts/funcionario.sql
@@ -0,0 +1,14 @@
+create table funcionario (
+	cod_funcionario INT PRIMARY KEY,
+	nome VARCHAR(50),
+	cpf VARCHAR(50),
+	sexo VARCHAR(50)
+);
+insert into funcionario (cod_funcionario, nome, cpf, sexo) values (1, 'Donalt Reaper', '632.339.945-68', 'M');
+insert into funcionario (cod_funcionario, nome, cpf, sexo) values (2, 'Gwenore Decourcy', '846.411.238-28', 'F');
+insert into funcionario (cod_funcionario, nome, cpf, sexo) values (3, 'Zorah Rummin', '099.715.761-93', 'F');
+insert into funcionario (cod_funcionario, nome, cpf, sexo) values (4, 'Ewell Norridge', '260.625.460-12', 'M');
+insert into funcionario (cod_funcionario, nome, cpf, sexo) values (5, 'Nissy Cobleigh', '431.519.639-48', 'F');
+insert into funcionario (cod_funcionario, nome, cpf, sexo) values (6, 'Lois Conrath', '609.827.138-95', 'F');
+insert into funcionario (cod_funcionario, nome, cpf, sexo) values (7, 'Lydia Gley', '202.637.627-10', 'F');
+insert into funcionario (cod_funcionario, nome, cpf, sexo) values (8, 'Winne Ashburner', '515.422.931-12', 'F');
