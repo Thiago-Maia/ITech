49f2e3e95a17eeab9c25a4e543eab0dff9b8750a
Thiago-Maia <72457726+Thiago-Maia@users.noreply.github.com>
2021-11-25 01:35:57 -0300

    Add files via upload

---
+++ Itech/scripts/script transacional/fornecedor.sql
@@ -0,0 +1,17 @@
+create table fornecedor (
+	cod_fornecedor INT PRIMARY KEY,
+	nome VARCHAR(50),
+	cnpj VARCHAR(50)
+);
+insert into fornecedor (cod_fornecedor, nome, cnpj) values (1, 'Simon Property Group, Inc.', '83.060.498\0001-65');
+insert into fornecedor (cod_fornecedor, nome, cnpj) values (2, 'ChinaCache International Holdings Ltd.', '04.043.217\0001-97');
+insert into fornecedor (cod_fornecedor, nome, cnpj) values (3, 'Unilever PLC', '44.594.081\0001-45');
+insert into fornecedor (cod_fornecedor, nome, cnpj) values (4, 'Eagle Point Credit Company Inc.', '00.288.495\0001-25');
+insert into fornecedor (cod_fornecedor, nome, cnpj) values (5, 'Pan American Silver Corp.', '29.735.003\0001-38');
+insert into fornecedor (cod_fornecedor, nome, cnpj) values (6, 'Numerex Corp.', '59.338.481\0001-37');
+insert into fornecedor (cod_fornecedor, nome, cnpj) values (7, 'Progress Software Corporation', '75.956.619\0001-05');
+insert into fornecedor (cod_fornecedor, nome, cnpj) values (8, 'Dollar Tree, Inc.', '44.297.338\0001-99');
+insert into fornecedor (cod_fornecedor, nome, cnpj) values (9, 'Compugen Ltd.', '06.354.896\0001-83');
+insert into fornecedor (cod_fornecedor, nome, cnpj) values (10, 'Francesca''s Holdings Corporation', '56.594.096\0001-45');
+insert into fornecedor (cod_fornecedor, nome, cnpj) values (11, 'Columbia Property Trust, Inc.', '25.405.759\0001-30');
+insert into fornecedor (cod_fornecedor, nome, cnpj) values (12, 'Guggenheim Strategic Opportunities Fund', '53.041.340\0001-48');
